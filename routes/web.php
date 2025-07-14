bd08c25e23aa74310634684892199d906b0f4120
caremi <Willy Nilly>
2025-07-14 22:18:16 +0400

    implement route RESTFull resource

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,19 @@ Route::prefix('admin')->middleware(['auth'])->group(function () {
 Route::middleware([App\Http\Middlewares\AuthMiddleware::class])->group(function () {
     Route::get('/dashboard', DashboardController::class, 'index');
 });
+
+
+// Basic resource
+Route::resource('posts', PostController::class);
+
+// Only a few methods
+Route::resource('users', \App\Http\Controllers\User\UserController::class);
+
+// Only a few methods
+Route::resource('users', \App\Http\Controllers\User\UserController::class)->only(['index', 'show']);
+
+// Exclude some
+Route::resource('users', \App\Http\Controllers\User\UserController::class)->except(['create', 'edit']);
+
+// Add middleware to all routes
+Route::resource('products', PostController::class)->middleware([App\Http\Middlewares\AuthMiddleware::class]);
